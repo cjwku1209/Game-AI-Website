fe5c8f65928a5ea3646ff7c77b81ce78ba7b7db8
Calvin Ku <calvinku@gapp.nthu.edu.tw>
2021-10-30 16:34:37 +0800

    refinement

--- gameai.js
+++ gameai.js
@@ -3,7 +3,14 @@ $(document).ready(function(){
     $('#myTabs a').click(function (e) {
         e.preventDefault()
         $(this).tab('show')
-      })
+    })
+    $('#sign-up-button').click(function (e) {
+        window.open("https://forms.gle/57p5mzEPXEFmuzz97")
+    })
+    $('#sign-up').click(function (e) {
+        window.open("https://forms.gle/57p5mzEPXEFmuzz97")
+    })
+    // https://forms.gle/57p5mzEPXEFmuzz97
 });
 
 var scrollSpy = new bootstrap.ScrollSpy(document.body, {
